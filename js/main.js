16f0f402c518b3346a674aa49093e9f3c3e60322
Hayley McDonald <hhmcdonald@gmail.com>
2016-08-05 18:35:23 -0500

    give a smooth scroll function to the page navigation

--- js/main.js
+++ js/main.js
@@ -5,4 +5,17 @@ $(document).ready(function () {
         prependTo: '#demo1'
     });
 
+	// jQuery for smooth scroll
+    $('a').on('click', function (event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
 });
